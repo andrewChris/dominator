c28045a92819d718b5c1722db7ef6c0f0596995c
Andreas <a.j.christophides@accenture.com>
2016-03-09 15:47:27 +1100

    first commit

---
+++ Dominator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dominator
+//
+//  Created by Christophides, A. J. on 9/03/2016.
+//  Copyright © 2016 The Dominator Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dominator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dominator
+//
+//  Created by Christophides, A. J. on 9/03/2016.
+//  Copyright © 2016 The Dominator Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
