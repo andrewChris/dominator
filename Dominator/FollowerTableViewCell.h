270e45d59545f49fca7fbf9b5c6e25b5f4ba9771
Andreas <a.j.christophides@accenture.com>
2016-03-10 09:27:27 +1100

    commit new cells

---
+++ Dominator/FollowerTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FollowerTableViewCell.h
+//  Dominator
+//
+//  Created by Christophides, A. J. on 10/03/2016.
+//  Copyright © 2016 The Dominator Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FollowerTableViewCell : UITableViewCell
+
+@end
